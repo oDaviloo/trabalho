b63b772958533963ee031720a76c1c2230f880ab
Davi Assunção Lourenço <davilo905@gmail.com>
2023-11-30 20:21:52 -0400

    login cadastro ai

---
+++ app.js
@@ -0,0 +1,185 @@
+
+
+
+const express = require('express');
+const session = require("express-session");
+const path = require('path');
+const app = express();
+
+const bodyParser = require("body-parser");
+
+const mysql = require("mysql"); 
+const { resolveSoa } = require('dns');
+
+app.use(session({secret: "ssshhhhh", resave: false, // Adicione esta linha para resolver o aviso de depreciação
+saveUninitialized: false // Adicione esta linha para resolver o aviso de depreciação
+}));
+
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+//conexão com banco mysql
+function conectiondb(){
+    var con = mysql.createConnection({
+        host: 'localhost', 
+        user: 'root', 
+        password: '', 
+        database: 'dblogin' 
+    });
+
+   
+    con.connect((err) => {
+        if (err) {
+            console.log('Erro connecting to database...', err)
+            return
+        }
+        console.log('Connection established!')
+    });
+
+    return con;
+}
+
+
+
+app.get('/', (req, res) => {
+    var message = ' ';
+    req.session.destroy();
+    res.render('views/registro', { message: message });
+});
+
+
+
+app.get('/views/registro', (req, res)=>{
+    res.redirect('../');
+    
+});
+
+
+app.get("/views/home", function (req, res){
+    
+  
+    if (req.session.user){
+        var con = conectiondb();
+        var query2 = 'SELECT * FROM users WHERE email LIKE ?';
+        con.query(query2, [req.session.user], function (err, results){
+            res.render('views/home', {message:results});
+            
+        });
+        
+    }else{
+        res.redirect("/");
+    }
+    
+});
+
+//rota para login
+app.get("/views/login", function(req, res){
+    var message = ' ';
+    res.render('views/login', {message:message});
+});
+
+//método post do register
+app.post('/register', function (req, res){
+
+    var username = req.body.nome;
+    var pass = req.body.pwd;
+    var email = req.body.email;
+    var idade = req.body.idade;
+
+    var con = conectiondb();
+
+    var queryConsulta = 'SELECT * FROM users WHERE email LIKE ?';
+
+    con.query(queryConsulta, [email], function (err, results){
+        if (results.length > 0){            
+            var message = 'E-mail já cadastrado';
+            res.render('views/registro', { message: message });
+        }else{
+            var query = 'INSERT INTO users VALUES (DEFAULT, ?, ?, ?, ?)';
+
+            con.query(query, [username, email, idade, pass], function (err, results){
+                if (err){
+                    throw err;
+                }else{
+                    console.log ("Usuario adicionado com email " + email);
+                    var message = "ok";
+                    res.render('views/registro', { message: message });
+                }        
+            });
+        }
+    });
+});
+
+
+app.post('/log', function (req, res){
+ 
+    var email = req.body.email;
+    var pass = req.body.pass;
+   
+    var con = conectiondb();
+   
+    var query = 'SELECT * FROM users WHERE pass = ? AND email LIKE ?';
+    
+    
+    con.query(query, [pass, email], function (err, results){
+        if (results.length > 0){
+            req.session.user = email;         
+            console.log("Login feito com sucesso!");
+            res.render('views/home', {message:results});
+        }else{
+            var message = 'Login incorreto!';
+            res.render('views/login', { message: message });
+        }
+    });
+});
+
+app.post('/update', function (req, res){
+   
+    console.log("entrou");
+    
+    var email = req.body.email;
+    var pass = req.body.pwd;
+    var username = req.body.nome;
+    var idade = req.body.idade;
+   
+    var con = conectiondb();
+   
+    var query = 'UPDATE users SET username = ?, pass = ?, idade = ? WHERE email LIKE ?';
+    
+
+   
+    con.query(query, [username, pass, idade, req.session.user], function (err, results){
+        
+        var query2 = 'SELECT * FROM users WHERE email LIKE ?';
+        con.query(query2, [req.session.user], function (err, results){
+            res.render('views/home', {message:results});    
+        });
+        
+    });
+});
+
+app.post('/delete', function (req, res){
+    
+    
+    var username = req.body.nome;
+    
+   
+    var con = conectiondb();
+   
+    var query = 'DELETE FROM users WHERE email LIKE ?';
+    
+
+    
+    con.query(query, [req.session.user], function (err, results){
+        res.redirect ('/');
+    });
+});
+
+//executa servidor
+app.listen(8081, () => console.log(`App listening on port!`));
