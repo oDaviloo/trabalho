c3457e6f6dfa27c8935deac0db66a4cfde3bfbab
Davi Assunção Lourenço <davilo905@gmail.com>
2023-12-03 05:58:24 -0400

    Toda a página de perfil empresa

--- app.js
+++ app.js
@@ -3,34 +3,32 @@ const session = require("express-session");
 const path = require('path');
 const app = express();
 const axios = require('axios');
-
 const bodyParser = require("body-parser");
-
-const mysql = require("mysql"); 
-const { resolveSoa } = require('dns');
+const mysql = require("mysql");
 let connection;
 
-app.use(session({secret: "ssshhhhh", resave: false, 
-saveUninitialized: false 
+app.use(session({
+    secret: "ssshhhhh",
+    resave: false,
+    saveUninitialized: false
 }));
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/public'));
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-//conexão com banco mysql
-function conectiondb(){
+// Função para estabelecer conexão com o banco de dados
+function conectiondb() {
     if (!connection || !connection.threadId) {
         connection = mysql.createConnection({
-            host: 'localhost', 
-            user: 'root', 
-            password: '', 
-            database: 'caçaofertas' 
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'caçaofertas'
         });
 
         connection.connect((err) => {
@@ -38,108 +36,153 @@ function conectiondb(){
                 console.log('Erro connecting to database...', err);
                 return;
             }
-            console.log('Connection established!')
+            console.log('Connection established!');
         });
     }
 
     return connection;
 }
 
-
+// Rota raiz - renderiza a página de registro
 app.get('/', (req, res) => {
     var message = ' ';
     req.session.destroy();
     res.render('views/registro', { message: message });
 });
 
-
-
-app.get('/views/registro', (req, res)=>{
+// Rota para redirecionar a página de registro
+app.get('/views/registro', (req, res) => {
     res.redirect('../');
-    
 });
 
-
-app.get("/views/home", function (req, res){
-    
-  
-    if (req.session.user){
+// Rota para redirecionar a página de home após login
+app.get("/views/home", function (req, res) {
+    if (req.session.user) {
         var con = conectiondb();
         var query2 = 'SELECT * FROM users WHERE email LIKE ?';
-        con.query(query2, [req.session.user], function (err, results){
-            res.render('views/home', {message:results});
-            
+        con.query(query2, [req.session.user], function (err, results) {
+            res.render('views/home', { message: results });
         });
-        
-    }else{
+    } else {
         res.redirect("/");
     }
-    
 });
 
-
-//rota para login
-app.get("/views/login", function(req, res){
+// Rota para a página de login
+app.get("/views/login", function (req, res) {
     var message = ' ';
-    res.render('views/login', {message:message});
+    res.render('views/login', { message: message });
 });
 
-app.get('/cadastro_empresas', function(req, res) {
-    // Lógica para renderizar a página de cadastro de empresas
-    res.render('views/cadastro_empresas');
+// tudo de perfil empresa
+
+// Rota para o perfil da empresa
+app.get('/perfil_empresa', function (req, res) {
+    if (req.session.user) {
+        const con = conectiondb();
+        const query = 'SELECT * FROM empresas WHERE email LIKE ?';
+        con.query(query, [req.session.user], function (err, results) {
+            if (err) {
+                console.error('Erro ao obter perfil da empresa:', err);
+                res.status(500).send('Erro ao obter perfil da empresa. Tente novamente mais tarde.');
+            } else {
+                const profileData = results[0]; // Dados do perfil da empresa
+                const cep = profileData.cep; // Capturando o CEP da empresa
+
+                // Renderizando a página 'perfil_empresa' e passando os dados para o template
+                res.render('views/perfil_empresa', {
+                    profileData: profileData, // Dados do perfil da empresa
+                    cep: cep,
+                    logradouro: profileData.logradouro,
+                    complemento: profileData.complemento,
+                    bairro: profileData.bairro,
+                    localidade: profileData.localidade,
+                    nome_empresa: profileData.nome_empresa
+                    // Adicione aqui os outros campos necessários do perfil da empresa
+                });
+            }
+        });
+    } else {
+        res.redirect("/"); // Redirecionar se o usuário não estiver logado
+    }
 });
 
-// Método post do register para usuários
-app.post('/register', function (req, res) {
-    var username = req.body.nome;
-    var pass = req.body.pwd;
-    var email = req.body.email;
-    var idade = req.body.idade;
-    var cidade = req.body.cidade;
-    var estado = req.body.estado;
+// Rota para atualizar informações da empresa
+app.post('/atualizar_empresa', async (req, res) => { // Adicionando a palavra-chave async aqui
+    const { nomeEmpresa, cnpj, setor, senha, cep, numeroContato, numeroEndereco } = req.body;
 
-    var con = conectiondb();
+    // Verificar se todos os campos obrigatórios estão preenchidos
+   
 
-    // Verificação se o email já existe na tabela 'users'
-    var queryUser = 'SELECT * FROM users WHERE email LIKE ?';
+    try {
+        // Log para capturar o CEP utilizado na requisição
+        console.log('CEP utilizado:', cep);
 
-    con.query(queryUser, [email], function (err, resultsUser) {
-        if (resultsUser.length > 0) {
-            var message = 'E-mail já cadastrado como usuário';
-            res.render('views/registro', { message: message });
+        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json`);
+        const endereco = response.data;
+
+        // Certifique-se de que a conexão está estabelecida antes de usar connection.query
+        const connection = conectiondb();
+
+        // Atualizar informações da empresa no banco de dados
+        const query = `UPDATE empresas 
+                        SET nome_empresa = ?, cnpj = ?, setor = ?, senha = ?, cep = ?, 
+                            logradouro = ?, complemento = ?, bairro = ?, localidade = ?, 
+                            uf = ?, ibge = ?, gia = ?, ddd = ?, siafi = ?, numero_contato = ?, numero_endereco = ?
+                        WHERE email = ?`;
+
+        const queryParams = [
+            nomeEmpresa, cnpj, setor, senha, cep, endereco.logradouro, endereco.complemento, endereco.bairro,
+            endereco.localidade, endereco.uf, endereco.ibge, endereco.gia, endereco.ddd, endereco.siafi,
+            numeroContato, numeroEndereco, req.session.user
+        ];
+
+        connection.query(query, queryParams, (error, results) => {
+            if (error) {
+                console.error('Erro ao atualizar informações da empresa:', error);
+                res.status(500).send('Erro ao atualizar informações da empresa. Tente novamente mais tarde.');
+            } else {
+                res.redirect('/perfil_empresa'); // Redirecionar após a atualização bem-sucedida
+            }
+        });
+
+    } catch (error) {
+        console.error('Erro ao consultar CEP:', error.message);
+        res.status(500).send('Erro ao consultar CEP. Tente novamente mais tarde.');
+    }
+});
+
+// Rota para excluir perfil da empresa
+app.post('/excluir_empresa', function (req, res) {
+    const con = conectiondb();
+    const query = 'DELETE FROM empresas WHERE email LIKE ?';
+    con.query(query, [req.session.user], function (err, results) {
+        if (err) {
+            console.error('Erro ao excluir perfil da empresa:', err);
+            res.status(500).send('Erro ao excluir perfil da empresa. Tente novamente mais tarde.');
         } else {
-            // Se o email não existe na tabela 'users', então verifica na tabela 'empresas'
-            var queryCompany = 'SELECT * FROM empresas WHERE email LIKE ?';
-
-            con.query(queryCompany, [email], function (err, resultsCompany) {
-                if (resultsCompany.length > 0) {
-                    var message = 'E-mail já cadastrado como empresa';
-                    res.render('views/registro', { message: message });
-                } else {
-                    // Se o email não existe em nenhuma das tabelas, realiza o cadastro na tabela 'users'
-                    var query = 'INSERT INTO users (username, email, idade, cidade, estado, pass, tipo_conta) VALUES (?, ?, ?, ?, ?, ?, ?)';
-
-                    con.query(query, [username, email, idade, cidade, estado, pass, 'user'], function (err, results) {
-                        if (err) {
-                            throw err;
-                        } else {
-                            console.log("Usuário adicionado com email " + email);
-                            var message = "ok";
-                            res.render('views/registro', { message: message });
-                        }
-                    });
-                }
-            });
+            req.session.destroy();
+            res.redirect('/'); // Redirecionar após a exclusão do perfil
         }
     });
 });
 
 
-// cadastro empresas
+// tudo de perfil empresa FIM
 
-// Rota para cadastrar uma empresa
+
+
+// Rota para a página de cadastro de empresas
+app.get('/cadastro_empresas', function (req, res) {
+    // Lógica para renderizar a página de cadastro de empresas
+    res.render('views/cadastro_empresas');
+});
+
+// Método POST para cadastrar empresas
 app.post('/cadastrar_empresas', async (req, res) => {
-    const { nomeEmpresa, cnpj, setor, email, senha, cep } = req.body;
+    const { nomeEmpresa, cnpj, setor, email, senha, cep, numeroContato, numeroEndereco } = req.body;
+
+   
 
     try {
         // Log para capturar o CEP utilizado na requisição
@@ -176,19 +219,22 @@ app.post('/cadastrar_empresas', async (req, res) => {
                     gia: endereco.gia,
                     ddd: endereco.ddd,
                     siafi: endereco.siafi,
-                    tipo_conta: 'company' // Definindo o tipo de conta como 'company' para empresas
+                    tipo_conta: 'company', // Definindo o tipo de conta como 'company' para empresas
+                    numero_contato: numeroContato,
+                    numero_endereco: numeroEndereco
                 }, (error, results, fields) => {
                     if (error) {
                         console.error('Erro ao cadastrar empresa:', error);
-                        res.status(500).send('Erro ao cadastrar empresa. Por favor, tente novamente.');
+                        res.status(500).json({ success: false, message: 'Erro ao cadastrar empresa. Por favor, tente novamente.' });
                     } else {
                         console.log('Empresa cadastrada com sucesso!');
-                        res.redirect('/views/login'); // Redirecionar para a página de login após o cadastro bem-sucedido
+                        // Exibindo a mensagem de sucesso sem redirecionamento
+                        res.render('views/login', { message: 'Cadastro realizado com sucesso. Faça login para entrar.' });
+
                     }
                 });
             }
         });
-
     } catch (error) {
         console.error('Erro ao consultar CEP:', error.message);
         res.status(500).send('Erro ao consultar CEP. Por favor, tente novamente.');
@@ -197,9 +243,7 @@ app.post('/cadastrar_empresas', async (req, res) => {
 
 
 
-//cadastro empresas fim
-
-
+// Rota para o login
 app.post('/home', function (req, res) {
     var email = req.body.email;
     var pass = req.body.pass;
@@ -245,50 +289,38 @@ app.post('/home', function (req, res) {
     });
 });
 
-
-app.post('/update', function (req, res){
-   
-    console.log("entrou");
-    
+// Rota para atualização de informações de usuário
+app.post('/update', function (req, res) {
     var email = req.body.email;
     var pass = req.body.pwd;
     var username = req.body.nome;
     var idade = req.body.idade;
     var cidade = req.body.cidade;
     var estado = req.body.estado;
-   
+
     var con = conectiondb();
-   
+
     var query = 'UPDATE users SET username = ?, pass = ?, idade = ? WHERE email LIKE ?';
-    
 
-   
-    con.query(query, [username, pass, idade, req.session.user], function (err, results){
-        
+    con.query(query, [username, pass, idade, req.session.user], function (err, results) {
         var query2 = 'SELECT * FROM users WHERE email LIKE ?';
-        con.query(query2, [req.session.user], function (err, results){
-            res.render('views/home', {message:results});    
+        con.query(query2, [req.session.user], function (err, results) {
+            res.render('views/home', { message: results });
         });
-        
     });
 });
 
-app.post('/delete', function (req, res){
-    
-    
+// Rota para exclusão de conta de usuário
+app.post('/delete', function (req, res) {
     var username = req.body.nome;
-    
-   
     var con = conectiondb();
-   
     var query = 'DELETE FROM users WHERE email LIKE ?';
-    
-
-    
-    con.query(query, [req.session.user], function (err, results){
-        res.redirect ('/');
+    con.query(query, [req.session.user], function (err, results) {
+        res.redirect('/');
     });
 });
 
-//executa servidor
-app.listen(8081, () => console.log(`App listening on port!`));
+// Executa o servidor
+app.listen(8081, () => console.log(`App listening on port 8081!`));
+
+
